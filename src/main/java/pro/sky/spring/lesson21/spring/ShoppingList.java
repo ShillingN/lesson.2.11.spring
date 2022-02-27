6581a4d77588acc3741ba469b694a741acc7a81e
pro100 <pro100tkach@mail.ru>
2022-02-27 17:33:23 +0300

    lesson2.11 done 1

--- src/main/java/pro/sky/spring/lesson21/spring/ShoppingList.java
+++ src/main/java/pro/sky/spring/lesson21/spring/ShoppingList.java
@@ -1,21 +1,32 @@
-//package pro.sky.spring.lesson21.spring;
-//
-//import org.springframework.stereotype.Repository;
-//import org.springframework.web.context.annotation.SessionScope;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@SessionScope
-//@Repository
-//public class ShoppingList {
-//
-//    private final List<Integer> shoppingList;
-//
-//    public ShoppingList() {
-//        this.shoppingList = new ArrayList<>();
-//        shoppingList.add(1);
-//    }
-//
-//
-//}
+package pro.sky.spring.lesson21.spring;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.web.context.annotation.SessionScope;
+import pro.sky.spring.lesson21.spring.data.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SessionScope
+@Repository
+public class ShoppingList {
+
+    private final List<List> shoppingList;
+
+    public ShoppingList() {
+        this.shoppingList = new ArrayList<>();
+    }
+
+    public Product add(List productId) {
+        Product product = new Product(productId);
+        shoppingList.add(productId);
+        return product;
+    }
+
+    public List<List> getShoppingList() {
+        return shoppingList;
+    }
+
+
+
+}
--- src/main/java/pro/sky/spring/lesson21/spring/service/ProductServiceImpl.java
+++ src/main/java/pro/sky/spring/lesson21/spring/service/ProductServiceImpl.java
@@ -1,18 +1,26 @@
-//package pro.sky.spring.lesson21.spring.service;
-//
-//import org.springframework.stereotype.Service;
-//import pro.sky.spring.lesson21.spring.data.Product;
-//
-//@Service
-//public class ProductServiceImpl implements ProductService {
-//
-//    @Override
-//    public Product add(Integer productId) {
-//        return null;
-//    }
-//
-//    @Override
-//    public Product get(Integer productId) {
-//        return null;
-//    }
-//}
+package pro.sky.spring.lesson21.spring.service;
+
+import org.springframework.stereotype.Service;
+import pro.sky.spring.lesson21.spring.ShoppingList;
+import pro.sky.spring.lesson21.spring.data.Product;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ShoppingList shoppingList;
+
+    public ProductServiceImpl(ShoppingList shoppingList) {
+        this.shoppingList = shoppingList;
+    }
+
+    @Override
+    public Product add(List productId) {
+        return shoppingList.add(productId);
+    }
+
+    public List<List> get() {
+        return shoppingList.getShoppingList();
+    }
+}
